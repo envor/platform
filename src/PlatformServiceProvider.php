cd4ff138e4cde64a76ce2952c2d8515dfb1bbdd2
= <inman.turbo@gmail.com>
2024-02-19 19:12:49 -0500

    work in progress

--- src/PlatformServiceProvider.php
+++ src/PlatformServiceProvider.php
@@ -21,27 +21,24 @@ class PlatformServiceProvider extends PackageServiceProvider
             ->hasConfigFile(['database', 'auth', 'platform']);
     }
 
-
     public function packageBooted()
     {
 
         $this->app->booted(function () {
 
-            if(class_exists('\Livewire\Volt\Volt')) {
+            if (class_exists('\Livewire\Volt\Volt')) {
 
-                
                 $voltPaths = collect(\Livewire\Volt\Volt::paths())->map(function ($path) {
                     return $path->path;
                 })->toArray();
-                
+
                 $paths = array_merge($voltPaths, [
                     __DIR__.'/../resources/views/livewire',
                     __DIR__.'/../resources/views/pages',
                 ]);
-                
+
                 \Livewire\Volt\Volt::mount($paths);
             }
         });
     }
-
 }
